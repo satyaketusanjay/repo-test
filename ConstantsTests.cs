bb02b4b0af5e2d0cb573235bd701bbd4e2b74347
Satyaketu Sanjay <satya.tiwari@outlook.in>
2025-07-21 21:33:22 +0530

    Update ConstantsTests.cs

--- ConstantsTests.cs
+++ ConstantsTests.cs
@@ -1,102 +1,77 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using GPI.TransactionRecon.Logger;
+using GPI.TransactionRecon.BusinessLogic;
+using System.Collections.Generic;
 
 namespace GPI.TransactionRecon.Tests
 {
     [TestClass]
-    public class AppConfigurationTests
+    public class SFTPRetryDataTests
     {
-        private AppConfiguration _config;
-
-        [TestInitialize]
-        public void Setup()
+        [TestMethod]
+        public void SFTPRetryCount_GetSet_ShouldWorkCorrectly()
         {
-            _config = new AppConfiguration();
+            var retryData = new SFTPRetryData();
+            retryData.SFTPRetryCount = 5;
+            Assert.AreEqual(5, retryData.SFTPRetryCount);
         }
 
         [TestMethod]
-        public void All_AppSettings_Properties_ShouldNotBeNullOrEmpty()
+        public void SFTPFlag_GetSet_ShouldWorkCorrectly()
         {
-            Assert.IsFalse(string.IsNullOrEmpty(_config.XMLFileFormat));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.CSVFileFormat));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.TXTFileFormat));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.EXCEL));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.SmtpServer));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.TRMailFrom));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.TRMailTo));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.TRMailSubject));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.ScheduledTime));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.TRReportLocation));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.TRLogsPath));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.TRResourcePath));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.TRReport));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.TREODRReportMailTo));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.TREODRReportMailFrom));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.TREODRReportcountryManagers));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.TREODRReportSubject));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.TREODRReportBody));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.IsTREnabled));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.TRIgnoreStatusList));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.ParallelProcessFlag));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.EPD_system));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.SFTP_Systems));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.SFTPRetryCount));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.SFTPRetryDelay));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.SFTPSuccessMessage));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.SFTPErrorMessage));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.SFTPErrorEmailTo));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.IPAdd));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.TRThresholdAlertSubject));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.GpiOnlineURL));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.AcceptableFileFormats));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.BU_ID));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.GPI_Systems));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.Ignore_status_BU));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.IgnoreStatusList_Approved));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.IgnoreStatusList_Unapproved));
-            Assert.IsFalse(string.IsNullOrEmpty(_config.DatabaseConnectionString));
+            var retryData = new SFTPRetryData();
+            retryData.SFTPFlag = true;
+            Assert.IsTrue(retryData.SFTPFlag);
+
+            retryData.SFTPFlag = false;
+            Assert.IsFalse(retryData.SFTPFlag);
         }
 
         [TestMethod]
-        public void GetAppSetting_ValidKey_ShouldReturnValue()
+        public void epdCountFlag_GetSet_ShouldWorkCorrectly()
         {
-            var value = _config.GetAppSetting("XMLFileFormat");
-            Assert.AreEqual("*.xml", value);
+            var retryData = new SFTPRetryData();
+            var eventData = new OnEventData
+            {
+                BusinessUnit = "BU1",
+                SourceSystem = "SYS1"
+            };
+
+            var dict = new Dictionary<OnEventData, int>(new OnEventDataComparer())
+            {
+                { eventData, 3 }
+            };
+
+            retryData.epdCountFlag = dict;
+
+            Assert.IsNotNull(retryData.epdCountFlag);
+            Assert.AreEqual(3, retryData.epdCountFlag[eventData]);
         }
 
         [TestMethod]
-        public void GetAppSetting_InvalidKey_ShouldReturnNull()
+        public void OnEventData_GetSet_ShouldWorkCorrectly()
         {
-            var value = _config.GetAppSetting("NonExistentKey");
-            Assert.IsNull(value);
+            var eventData = new OnEventData
+            {
+                BusinessUnit = "BU123",
+                SourceSystem = "SYS456"
+            };
+
+            Assert.AreEqual("BU123", eventData.BusinessUnit);
+            Assert.AreEqual("SYS456", eventData.SourceSystem);
+        }
+    }
+
+    // Optional: Comparer for Dictionary key equality
+    public class OnEventDataComparer : IEqualityComparer<OnEventData>
+    {
+        public bool Equals(OnEventData x, OnEventData y)
+        {
+            return x.BusinessUnit == y.BusinessUnit && x.SourceSystem == y.SourceSystem;
         }
 
-        [TestMethod]
-        public void GetSftpUser_ValidKey_ShouldReturnValue()
+        public int GetHashCode(OnEventData obj)
         {
-            var value = _config.GetSftpUser("ABA");
-            Assert.AreEqual("svc_latam_gpi", value);
-        }
-
-        [TestMethod]
-        public void GetSftpUser_InvalidKey_ShouldReturnEmpty()
-        {
-            var value = _config.GetSftpUser("UNKNOWN");
-            Assert.AreEqual(string.Empty, value);
-        }
-
-        [TestMethod]
-        public void GetSftpPassword_ValidKey_ShouldReturnValue()
-        {
-            var value = _config.GetSftpPassword("ABA");
-            Assert.AreEqual(string.Empty, value); // empty string in config
-        }
-
-        [TestMethod]
-        public void GetSftpPassword_InvalidKey_ShouldReturnEmpty()
-        {
-            var value = _config.GetSftpPassword("UNKNOWN");
-            Assert.AreEqual(string.Empty, value);
+            return (obj.BusinessUnit + obj.SourceSystem).GetHashCode();
         }
     }
 }
